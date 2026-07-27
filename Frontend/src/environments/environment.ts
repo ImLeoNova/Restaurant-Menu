88509c1e18275c8091ebc6dc37864b2171d0d526
MRHidden100 <adibariyanesmaeilzadeh@gmail.com>
2026-07-27 16:48:21 +0330

    Fix Err

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  websiteAPI: 'http://localhost:8080',
-  aiAPI: 'http://localhost:8080/api/user/ai',
+  websiteAPI: '',
+  aiAPI: '/api/user/ai',
 };
